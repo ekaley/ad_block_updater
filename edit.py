94590fa22b9c0169762f75cf44e926c574edac63
Ethan Kaley <ethan.kaley@emc.com>
2018-05-04 15:22:48 -0400

    change name and upload host.txt

--- edit.py
+++ edit.py
@@ -4,7 +4,7 @@ import re
 url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts"
 
 hosts = urllib2.urlopen(url)
-with open('out_hosts.txt', "wb") as outfile:
+with open('hosts.txt', "wb") as outfile:
     for line in hosts:
         if not re.search(r"#.*\n", line):
                 line = re.sub(r"[0-255]+.[0-255]+.[0-255]+.[0-255]+", "127.0.0.1", line)
