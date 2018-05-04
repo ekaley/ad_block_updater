787d8c421f5925c071e19b93347eebde842839ec
Ethan Kaley <ethan.kaley@emc.com>
2018-05-04 15:18:05 -0400

    :wq     ./stuff:/\
    Signed-off-by: Joe Gorse <joseph.gorseiii@emc.com>

---
+++ edit.py
@@ -0,0 +1,11 @@
+import urllib2
+import re
+
+url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts"
+
+hosts = urllib2.urlopen(url)
+with open('out_hosts.txt', "wb") as outfile:
+    for line in hosts:
+        if not re.search(r"#.*\n", line):
+                line = re.sub(r"[0-255]+.[0-255]+.[0-255]+.[0-255]+", "127.0.0.1", line)
+                outfile.write(line)
